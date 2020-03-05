5e671c6004ef1f9beb25c68bd06072c5726f7dd9
Melissa Wise <wise.melissa19@gmail.com>
2020-03-05 07:36:55 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,14 @@
+puts "Hello World"
+current_weather = "raining"
+
+case current_weather
+  when
+    "sunny"
+        puts "grab some sunscreen!"
+  when
+    "raining"
+      puts "grab an umbrella"
+  when
+      "snowing"
+      puts "bundle up"
+end
